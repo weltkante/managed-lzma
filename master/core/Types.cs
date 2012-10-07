a912c0ce63e6a1f1c6e1f2a8eacfaa135f0bc6eb
Tobias Käs <weltkante@users.noreply.github.com>
2012-10-07 18:09:38 +0000

    Added caching to the LZMA buffer allocator to allow reuse across encoders. This is a temporary solution and will be replaced by either reusing encoders or allowing custom memory caching implementations.

--- master/core/Types.cs
+++ master/core/Types.cs
@@ -110,6 +110,10 @@ namespace ManagedLzma.LZMA.Master
             public static readonly ISzAlloc BigAlloc = new ISzAlloc(200);
             public static readonly ISzAlloc SmallAlloc = new ISzAlloc(100);
 
+            private static Dictionary<long, List<byte[]>> Cache1 = new Dictionary<long, List<byte[]>>();
+            private static Dictionary<long, List<ushort[]>> Cache2 = new Dictionary<long, List<ushort[]>>();
+            private static Dictionary<long, List<uint[]>> Cache3 = new Dictionary<long, List<uint[]>>();
+
             private int mKind;
 
             private ISzAlloc(int kind)
@@ -125,18 +129,51 @@ namespace ManagedLzma.LZMA.Master
 
             public byte[] AllocBytes(object p, long size)
             {
+                lock(Cache1)
+                {
+                    List<byte[]> cache;
+                    if(Cache1.TryGetValue(size, out cache) && cache.Count > 0)
+                    {
+                        byte[] buffer = cache[cache.Count - 1];
+                        cache.RemoveAt(cache.Count - 1);
+                        return buffer;
+                    }
+                }
+
                 System.Diagnostics.Debug.WriteLine("Alloc byte size=" + size);
                 return new byte[size];
             }
 
             public ushort[] AllocUInt16(object p, long size)
             {
+                lock(Cache2)
+                {
+                    List<ushort[]> cache;
+                    if(Cache2.TryGetValue(size, out cache) && cache.Count > 0)
+                    {
+                        ushort[] buffer = cache[cache.Count - 1];
+                        cache.RemoveAt(cache.Count - 1);
+                        return buffer;
+                    }
+                }
+
                 System.Diagnostics.Debug.WriteLine("Alloc ushort size=" + size);
                 return new ushort[size];
             }
 
             public uint[] AllocUInt32(object p, long size)
             {
+                lock(Cache3)
+                {
+                    List<uint[]> cache;
+                    if(Cache3.TryGetValue(size, out cache) && cache.Count > 0)
+                    {
+                        uint[] buffer = cache[cache.Count - 1];
+                        cache.RemoveAt(cache.Count - 1);
+                        return buffer;
+                    }
+                }
+
                 System.Diagnostics.Debug.WriteLine("Alloc uint size=" + size);
                 return new uint[size];
             }
@@ -146,19 +183,49 @@ namespace ManagedLzma.LZMA.Master
                 // ignore
             }
 
-            public void FreeBytes(object p, byte[] address)
+            public void FreeBytes(object p, byte[] buffer)
             {
-                // ignore
+                if(buffer != null)
+                {
+                    lock(Cache1)
+                    {
+                        List<byte[]> cache;
+                        if(!Cache1.TryGetValue(buffer.Length, out cache))
+                            Cache1.Add(buffer.Length, cache = new List<byte[]>());
+
+                        cache.Add(buffer);
+                    }
+                }
             }
 
-            public void FreeUInt16(object p, ushort[] address)
+            public void FreeUInt16(object p, ushort[] buffer)
             {
-                // ignore
+                if(buffer != null)
+                {
+                    lock(Cache2)
+                    {
+                        List<ushort[]> cache;
+                        if(!Cache2.TryGetValue(buffer.Length, out cache))
+                            Cache2.Add(buffer.Length, cache = new List<ushort[]>());
+
+                        cache.Add(buffer);
+                    }
+                }
             }
 
-            public void FreeUInt32(object p, uint[] address)
+            public void FreeUInt32(object p, uint[] buffer)
             {
-                // ignore
+                if(buffer != null)
+                {
+                    lock(Cache3)
+                    {
+                        List<uint[]> cache;
+                        if(!Cache3.TryGetValue(buffer.Length, out cache))
+                            Cache3.Add(buffer.Length, cache = new List<uint[]>());
+
+                        cache.Add(buffer);
+                    }
+                }
             }
         }
 
