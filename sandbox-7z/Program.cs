fa3f109845ab0400cdc9841322e3c1a49cb0533b
Tobias Käs <weltkante@users.noreply.github.com>
2016-01-04 16:09:55 +0100

    Improve the example and comments for the unpacking code.

--- sandbox-7z/Program.cs
+++ sandbox-7z/Program.cs
@@ -173,5 +173,150 @@ namespace sandbox_7z
                 }
             }
         }
+
+        private static void UnpackArchive(string archiveFileName, string targetDirectory, string password = null)
+        {
+            UnpackArchive(archiveFileName, targetDirectory, password != null ? ManagedLzma.PasswordStorage.Create(password) : null);
+        }
+
+        private static void UnpackArchive(string archiveFileName, string targetDirectory, ManagedLzma.PasswordStorage password)
+        {
+            if (!File.Exists(archiveFileName))
+                throw new FileNotFoundException("Archive not found.", archiveFileName);
+
+            // Ensure that the target directory exists.
+            Directory.CreateDirectory(targetDirectory);
+
+            using (var archiveStream = new FileStream(archiveFileName, FileMode.Open, FileAccess.Read, FileShare.Read | FileShare.Delete))
+            {
+                var archiveMetadataReader = new ManagedLzma.SevenZip.FileModel.ArchiveFileModelMetadataReader();
+                var archiveFileModel = archiveMetadataReader.ReadMetadata(archiveStream, password);
+                var archiveMetadata = archiveFileModel.Metadata;
+
+                // Workaround: the current file metadata does not provide the relative path of the files, so we need to reconstruct it from the metadata
+                var nameMap = new Dictionary<ManagedLzma.SevenZip.Metadata.DecodedStreamIndex, string>();
+                InitializeNameMap(nameMap, archiveFileModel.RootFolder, string.Empty);
+
+                for (int sectionIndex = 0; sectionIndex < archiveMetadata.DecoderSections.Length; sectionIndex++)
+                {
+                    var sectionReader = new ManagedLzma.SevenZip.Reader.DecodedSectionReader(archiveStream, archiveMetadata, sectionIndex, password);
+                    var sectionFiles = archiveFileModel.GetFilesInSection(sectionIndex);
+
+                    // The section reader is constructed from metadata, if the counts do not match there must be a bug somewhere.
+                    System.Diagnostics.Debug.Assert(sectionFiles.Count == sectionReader.StreamCount);
+
+                    // The section reader iterates over all files in the section. NextStream advances the iterator.
+                    for (; sectionReader.CurrentStreamIndex < sectionReader.StreamCount; sectionReader.NextStream())
+                    {
+                        var fileMetadata = sectionFiles[sectionReader.CurrentStreamIndex];
+
+                        // The ArchiveFileModelMetadataReader we used above processes special marker nodes and resolves some conflicts
+                        // in the archive metadata so we don't have to deal with them. In these cases there will be no file metadata
+                        // produced and we should skip the stream. If you want to process these cases manually you should use a different
+                        // MetadataReader subclass or write your own subclass.
+                        if (fileMetadata == null)
+                            continue;
+
+                        // These asserts need to hold, otherwise there's a bug in the mapping the metadata reader produced.
+                        System.Diagnostics.Debug.Assert(fileMetadata.StreamIndex.SectionIndex == sectionIndex);
+                        System.Diagnostics.Debug.Assert(fileMetadata.StreamIndex.StreamIndex == sectionReader.CurrentStreamIndex);
+
+                        // Ensure that the target directory is created.
+                        var filename = Path.Combine(targetDirectory, nameMap[fileMetadata.StreamIndex]);
+                        Directory.CreateDirectory(Path.GetDirectoryName(filename));
+
+                        // NOTE: you can have two using-statements here if you want to be explicit about it, but disposing the
+                        //       stream provided by the section reader is not mandatory, it is owned by the the section reader
+                        //       and will be auto-closed when moving to the next stream or when disposing the section reader.
+                        using (var stream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.Delete))
+                            sectionReader.OpenStream().CopyTo(stream);
+                    }
+                }
+
+                // Create empty files and empty directories.
+                UnpackArchiveStructure(archiveFileModel.RootFolder, targetDirectory);
+            }
+        }
+
+        // Workaround: the current file metadata does not provide the relative path of the files, so we need to reconstruct it from the metadata
+        private static void InitializeNameMap(Dictionary<ManagedLzma.SevenZip.Metadata.DecodedStreamIndex, string> nameMap, ManagedLzma.SevenZip.FileModel.ArchivedFolder folder, string path)
+        {
+            foreach (var item in folder.Items)
+            {
+                var file = item as ManagedLzma.SevenZip.FileModel.ArchivedFile;
+                if (file != null && !file.StreamIndex.IsUndefined)
+                    nameMap[file.StreamIndex] = Path.Combine(path, file.Name);
+
+                var subfolder = item as ManagedLzma.SevenZip.FileModel.ArchivedFolder;
+                if (subfolder != null)
+                    InitializeNameMap(nameMap, subfolder, Path.Combine(path, subfolder.Name));
+            }
+        }
+
+        private static void UnpackArchiveStructure(ManagedLzma.SevenZip.FileModel.ArchivedFolder folder, string targetDirectory)
+        {
+            if (folder.Items.IsEmpty)
+            {
+                // Empty folders need to be created manually since the unpacking code doesn't try to write into it.
+                Directory.CreateDirectory(targetDirectory);
+            }
+            else
+            {
+                foreach (var item in folder.Items)
+                {
+                    var file = item as ManagedLzma.SevenZip.FileModel.ArchivedFile;
+                    if (file != null)
+                    {
+                        // Files without content are not iterated during normal unpacking so we need to create them manually.
+                        if (file.StreamIndex.IsUndefined)
+                        {
+                            System.Diagnostics.Debug.Assert(file.Length == 0); // If the file has no content then it length should be zero, otherwise something is wrong.
+
+                            using (var stream = new FileStream(Path.Combine(targetDirectory, file.Name), FileMode.Create, FileAccess.Write, FileShare.Delete))
+                            {
+                                // Nothing to do, FileMode.Create already truncates the file on opening.
+                            }
+                        }
+                    }
+
+                    var subfolder = item as ManagedLzma.SevenZip.FileModel.ArchivedFolder;
+                    if (subfolder != null)
+                        UnpackArchiveStructure(subfolder, Path.Combine(targetDirectory, subfolder.Name));
+                }
+            }
+        }
+
+        private static void SetFileAttributes(string path, ManagedLzma.SevenZip.FileModel.ArchivedFile file)
+        {
+            if (file.Attributes.HasValue)
+            {
+                // Not all attributes make sense to restore.
+                // - 'Directory' attribute - can't be changed by setting the attribute (and should already be correct anyways)
+                // - 'Device' attribute - not supported by 7z archives, we don't have enough data to restore this attribute
+                // - 'Normal' attribute - that attribute has special meaning for file creation and shouldn't appear in an archive
+                // - 'Temporary' attribute - while this attribute may happen to be set it makes no sense to restore it; your opinion may differ, so include it if you think that its needed, but make sure you know how Windows handles the attribute and test it properly!
+                // - 'SparseFile' attribute - sparse files require additional handling, just setting the attribute makes no sense
+                // - 'ReparsePoint' attribute - not supported by 7z archives, we don't have enough data to restore this attribute
+                // - 'Offline' attribute - I don't know how exactly this attribute works, but I think it is set by the OS and not supposed to be set by applications
+                // - 'IntegrityStream' attribute - not supported by 7z archives, we don't have enough data to restore this attribute
+                // - 'NoScrubData' attribute - no idea what that means, better not mess with it
+
+                // TODO: follow up with an actual list of attributes supported by the reference implementation
+                //       maybe we should make our own enum (providing conversions to/from FileAttributes enum)
+
+                const FileAttributes kAttrMask = default(FileAttributes)
+                    | FileAttributes.Archive
+                    | FileAttributes.ReadOnly
+                    | FileAttributes.Hidden
+                    //| FileAttributes.System -- not tested but probably would work
+                    //| FileAttributes.Compressed -- not tested if it is enough to just set the attribute
+                    //| FileAttributes.NotContentIndexed -- not tested but from the description it may make sense to restore it
+                    ;
+
+                var attr = File.GetAttributes(path);
+                attr = (attr & ~kAttrMask) | (file.Attributes.Value & kAttrMask);
+                File.SetAttributes(path, attr);
+            }
+        }
     }
 }
--- shared/SevenZip/ArchiveReader.cs
+++ shared/SevenZip/ArchiveReader.cs
@@ -9,6 +9,9 @@ using ManagedLzma.SevenZip.Metadata;
 
 namespace ManagedLzma.SevenZip.Reader
 {
+    /// <summary>
+    /// Provides an iterator-like API to unpack files from an archive section.
+    /// </summary>
     public sealed class DecodedSectionReader : IDisposable
     {
         #region Implementation
@@ -19,6 +22,13 @@ namespace ManagedLzma.SevenZip.Reader
         private DecodedStream mCurrentStream;
         private int mIndex;
 
+        /// <summary>
+        /// Creates a new iteration to unpack a sequence of streams from a decoded archive section.
+        /// </summary>
+        /// <param name="stream">A readable and seekable stream for the archive.</param>
+        /// <param name="metadata">The metadata for the archive.</param>
+        /// <param name="index">The decoder section index from the metadata which should be unpacked.</param>
+        /// <param name="password">An optional password to unpack the archive content.</param>
         public DecodedSectionReader(Stream stream, ArchiveMetadata metadata, int index, PasswordStorage password)
         {
             mDecodedStream = new ArchiveSectionDecoder(stream, metadata, index, password);
@@ -32,31 +42,50 @@ namespace ManagedLzma.SevenZip.Reader
             mDecodedStream.Dispose();
         }
 
+        /// <summary>
+        /// Returns the number of streams in the section.
+        /// </summary>
         public int StreamCount
         {
             get { return mDecoderSection.Streams.Length; }
         }
 
+        /// <summary>
+        /// Returns the index of the current stream in the section.
+        /// </summary>
         public int CurrentStreamIndex
         {
             get { return mIndex; }
         }
 
+        /// <summary>
+        /// Returns the position in the current stream.
+        /// </summary>
         public long CurrentStreamPosition
         {
             get { return mCurrentStream != null ? mCurrentStream.Position : 0; }
         }
 
+        /// <summary>
+        /// Returns the length of the current stream.
+        /// </summary>
         public long CurrentStreamLength
         {
             get { return mDecoderSection.Streams[mIndex].Length; }
         }
 
+        /// <summary>
+        /// Returns the checksum of the current stream, if available.
+        /// </summary>
         public Checksum? CurrentStreamChecksum
         {
             get { return mDecoderSection.Streams[mIndex].Checksum; }
         }
 
+        /// <summary>
+        /// Opens the current stream. Can only be called once per stream.
+        /// Disposing the returned stream is allowed but not required.
+        /// </summary>
         public Stream OpenStream()
         {
             if (this.CurrentStreamIndex == this.StreamCount)
@@ -70,6 +99,9 @@ namespace ManagedLzma.SevenZip.Reader
             return mCurrentStream;
         }
 
+        /// <summary>
+        /// Disposes the current stream and moves to the next stream.
+        /// </summary>
         public void NextStream()
         {
             if (this.CurrentStreamIndex == this.StreamCount)
