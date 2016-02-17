2c3a7106c67f99a6d5bd4b7a23f01c76394a0fe2
Tobias Käs <weltkante@users.noreply.github.com>
2016-02-17 20:19:42 +0100

    v0.2-alpha-5

--- library/Properties/AssemblyInfo.cs
+++ library/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ManagedLzma (Desktop46 v0.2-alpha-4)")]
+[assembly: AssemblyTitle("ManagedLzma (Desktop46 v0.2-alpha-5)")]
 [assembly: AssemblyDescription("Managed LZMA and SevenZip library.")]
 //[assembly: AssemblyConfiguration("")]
 //[assembly: AssemblyCompany("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
