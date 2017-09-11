1e21358a9a824fb61b0276e5fd87c4e2dee85ef7
Tobias Käs <weltkante@users.noreply.github.com>
2017-09-11 20:23:56 +0200

    Add AssemblyInfo.cs to .NET standard projects.

---
+++ standard13/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ManagedLzma (Standard13 v0.2-alpha-7)")]
+[assembly: AssemblyDescription("Managed LZMA and SevenZip library.")]
+[assembly: AssemblyProduct("ManagedLzma")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
